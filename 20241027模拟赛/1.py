09ed8cc94838005e3604e9b861c8d13a17d39f95
zhaoxinyi02 <98445030+zhaoxinyi02@users.noreply.github.com>
2024-12-03 16:59:10 +0800

    Delete 20241027模拟赛 directory

--- 20241027模拟赛/1.py
+++
@@ -1,8 +0,0 @@
-def func1(s,t):
-    if t == 0:
-        return
-    print(s[t-1],end='')
-    func1(s,t-2)
-s = input()
-l = len(s)
-func1(s,l)
--- 20241027模拟赛/3.py
+++
@@ -1,3 +0,0 @@
-nums = input()
-num1 = int(nums.split(' ')[0])
-num2 = int(nums.split(' ')[1])
