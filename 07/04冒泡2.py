fe6fb182c0bbbd6e795014bd624ac44e43bffa10
zhaoxinyi02 <98445030+zhaoxinyi02@users.noreply.github.com>
2024-12-03 16:58:44 +0800

    Delete 07 directory

--- 07/04冒泡2.py
+++
@@ -1,9 +0,0 @@
-def bubble(list):
-    l = len(list)
-    for i in range(0,l-1):
-        for j in range(0,l-1):
-            if list[j] < list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
-    print(list)
-list = [12,0,10,9,8,-1,21,-6]
-bubble(list)
